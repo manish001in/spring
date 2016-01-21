d0284e5867fc736edcb9a0d618e3122bf44445ac
Manish Rastogi <manish.rastogi@ishisystems.com>
2016-01-21 18:40:58 +0530

    Intro to Spring

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,10 @@
+/**
+ * Created by manish.rastogi on 8/1/16.
+ */
+package hello;
+
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package hello;
+
+/**
+ * Created by manish.rastogi on 8/1/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
